02e64db141c1c65a028c2151d931b711e6523fa1
ahsanmubashir01 <134580109+ahsanmubashir01@users.noreply.github.com>
2023-08-10 15:15:57 +0500

    ok

---
+++ lib/main_screens/homehome.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class homehome extends StatefulWidget {
+
+
+  @override
+  State<homehome> createState() => _homehomeState();
+}
+
+class _homehomeState extends State<homehome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: [
+      Text('data'),
+
+    ],),);
+  }
+}
