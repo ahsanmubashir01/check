183e9ab7bcd3f6fe1ee442b7410181e6067fb3f8
ahsanmubashir01 <134580109+ahsanmubashir01@users.noreply.github.com>
2023-08-10 15:21:16 +0500

    final testing

---
+++ lib/main_screens/okay.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class okay extends StatefulWidget {
+
+
+  @override
+  State<okay> createState() => _okayState();
+}
+
+class _okayState extends State<okay> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
